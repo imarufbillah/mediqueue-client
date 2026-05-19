8a8011ea04756b697b82a68b0fe0aff10c7af45b
Maruf Billah <marufbillah.dev@gmail.com>
2026-05-19 17:01:36 +0600

    feat(data): add getTutorsByCurrentUser function for fetching tutors associated with the logged-in user

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,4 +1,15 @@
+import { auth } from "./auth";
+import { headers } from "next/headers";
+
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+const jwtToken = async () => {
+  const tokenResponse = await auth.api.getToken({
+    headers: await headers(),
+  });
+
+  return tokenResponse.token;
+};
 
 // Get all tutors
 export const getTutors = async () => {
@@ -27,6 +38,21 @@ export const getTutorById = async (id) => {
 // Get limited tutors
 export const getLimitedTutors = async (limit = 6) => {
   const res = await fetch(`${API_BASE_URL}/tutors?limit=${limit}`);
+
+  if (!res.ok) {
+    const error = await res.json().catch(() => ({}));
+    throw new Error(error.message || "Failed to get tutors");
+  }
+
+  return res.json();
+};
+
+export const getTutorsByCurrentUser = async () => {
+  const res = await fetch(`${API_BASE_URL}/my-tutors`, {
+    headers: {
+      Authorization: `Bearer ${await jwtToken()}`,
+    },
+  });
 
   if (!res.ok) {
     const error = await res.json().catch(() => ({}));
